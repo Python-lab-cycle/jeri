c06f70392ff3ee5ce19ee59f18d5772c34df8dd4
jeri-cpu <76677031+jeri-cpu@users.noreply.github.com>
2021-01-11 20:10:56 +0530

    Add files via upload

---
+++ Gcd.py
@@ -0,0 +1,6 @@
+# GCD of two numbers
+a=int(input("Enter first number:"))
+b=int(input("Enter the second number:"))
+for i in range (1, min(a,b) +1):
+  if (a%i==0 and b%i==0): gcd=i
+print("GCD of",a,  "and",b,    "is",gcd)
