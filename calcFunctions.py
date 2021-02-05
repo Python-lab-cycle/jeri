f8ad2c48d55ee83726d0c0b9066bbdde872d5e18
jeri-cpu <76677031+jeri-cpu@users.noreply.github.com>
2021-02-05 22:10:10 +0530

    Add files via upload

---
+++ calcFunctions.py
@@ -0,0 +1,16 @@
+# This function adds two numbers
+def addition(x, y):
+    result = x+y
+    return result
+
+# This function subtracts two numbers
+def subtraction(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiplication(x, y):
+    return x * y
+
+# This function divides two numbers
+def division(x, y):
+    return float(x / y)
